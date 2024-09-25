da81ddc09ca8d56e2c242df017facd0454e4b466
liujiapeng <18829526908@163.com>
2024-09-25 15:29:28 +0800

    添加参数校验异常相关处理

--- rookie-im-api/im-auth/src/main/java/com/rookie/stack/im/auth/exception/AuthGlobalExceptionHandler.java
+++ rookie-im-api/im-auth/src/main/java/com/rookie/stack/im/auth/exception/AuthGlobalExceptionHandler.java
@@ -5,9 +5,12 @@ import com.rookie.stack.framework.common.exception.BusinessException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static com.rookie.stack.framework.common.exception.CommonErrorEnum.PARAM_INVALID;
 
 @RestControllerAdvice
 @Slf4j
@@ -24,5 +27,15 @@ public class AuthGlobalExceptionHandler {
     public ApiResult<?> exceptionHandler(Exception e) {
         log.info("business exception！The reason is：{}", e.getMessage(), e);
         return ApiResult.fail(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
+    }
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiResult<?> methodArgumentNotValidExceptionHandler(HttpServletRequest request, MethodArgumentNotValidException e) {
+        StringBuilder errMsg = new StringBuilder();
+        e.getBindingResult().getFieldErrors()
+                .forEach(x -> errMsg.append(x.getField()).append(x.getDefaultMessage()).append(","));
+        String message = errMsg.toString();
+
+        return ApiResult.fail(PARAM_INVALID.getCode(), message.substring(0, message.length() - 1));
     }
 }
