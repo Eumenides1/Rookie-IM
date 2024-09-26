bfda8a71fca2312c7c5bcd24fa99b1d7f2bb9c38
liujiapeng <18829526908@163.com>
2024-09-26 14:08:02 +0800

    引入gateway搭建网关模块

---
+++ rookie-im-api/im-gateway/src/main/java/com/rookie/stack/im/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.rookie.stack.im.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Classname GatewayApplication
+ * @Description TODO
+ * @Date 2024/9/26 13:57
+ * @Created by liujiapeng
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
