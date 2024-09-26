490be2e7640a62d10cafa58d9ec2b18169f604a7
liujiapeng <18829526908@163.com>
2024-09-26 13:44:58 +0800

    调整项目版本，兼容nacos

--- rookie-im-api/im-auth/src/main/java/com/rookie/stack/im/auth/controller/TestController.java
+++ rookie-im-api/im-auth/src/main/java/com/rookie/stack/im/auth/controller/TestController.java
@@ -1,8 +1,6 @@
 package com.rookie.stack.im.auth.controller;
 
 import com.alibaba.nacos.api.config.annotation.NacosValue;
-import com.rookie.stack.im.auth.alarm.AlarmInterface;
-import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,15 +16,6 @@ public class TestController {
     @NacosValue(value = "${rate-limit.api.limit}", autoRefreshed = true)
     private Integer limit;
 
-    @Resource
-    private AlarmInterface alarm;
-
-
-    @GetMapping("/alarm")
-    public String sendAlarm() {
-        alarm.send("系统出错啦，jaguarliu这个月绩效没了，速度上线解决问题！");
-        return "alarm success";
-    }
 
     @GetMapping("/test")
     public String test() {
